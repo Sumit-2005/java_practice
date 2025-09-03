0293c04360f6b87a5ba7577d28f7b3d9939cab66
Sumit Bhagat <sumitbhagat9266@gmail.com>
2025-09-03 23:06:41 +0530

    Practiced New Queue Implementations

--- queues_arrays.java
+++ queues_arrays.java
@@ -3,25 +3,35 @@ public class queues_arrays {
         static int arr[];
         static int size;
         static int rear;
+        static int front;
         
         Queue(int n){
             arr = new int[n];
             size = n;
             rear = -1;
+            front = -1;
         }
 
         public static boolean isEmpty(){
-            return rear == -1;
+            return rear == -1 && front == -1;
+        }
+
+        public static boolean isFull(){
+            return (rear+1)%size == front;
         }
 
         //add
         public static void add(int data){
-            if(rear == size-1){
+            if(isFull()){
                 System.out.println("Queue is full");
                 return;
             }
 
-            rear = rear+1;
+            //Adding First Element
+            if(front == -1){
+                front = 0;
+            }
+            rear = (rear+1)%size;
             arr[rear] = data;
         }
 
@@ -32,12 +42,13 @@ public class queues_arrays {
                 return -1;
             }
 
-            int front = arr[0];
-            for(int i=0; i<rear;i++){
-                arr[i] = arr[i+1];
-            }
-            rear = rear-1;
-            return front;
+            int result = arr[front];
+            if(rear == front){
+                rear = front = -1;
+            }else{
+                front = (front+1)% size;
+            } 
+            return result;
         }
 
         //peek
@@ -46,7 +57,7 @@ public class queues_arrays {
                 System.out.println("Queue is Empty");
                 return -1;
             }
-            return arr[0];
+            return arr[front];
         }
     }
 
