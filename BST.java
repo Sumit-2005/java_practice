3cc01469dfd5ddd0cfb51e4d065942be6b239d42
Sumit Bhagat <sumitbhagat9266@gmail.com>
2025-09-18 15:17:49 +0530

    Binary Search Tree Part 1 Completed

--- BST.java
+++ BST.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class BST {
     static class Node{
         int data;
@@ -49,20 +51,126 @@ public class BST {
             return search(root.right, key);
         }
     }
+
+    public static Node delete(Node root, int val){
+        if(root.data < val){
+            delete(root.right, val);
+        }else if(root.data > val){
+            delete(root.left, val);
+        }
+
+        if(root.data == val){
+            ///No Child
+            if(root.left == null && root.right == null){
+                return null;
+            }
+
+            //1 Child
+            if(root.left == null){
+                return root.right;
+            }else if(root.right == null){
+                return root.left;
+            }
+
+            //2 Children
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node inorderSuccessor(Node root){
+        while(root.left != null){
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void printInRange(Node root, int k1, int k2){
+        if(root == null){
+            return;
+        }
+        if(k1 <= root.data && root.data <= k2){
+            printInRange(root.left, k1, k2);
+            System.out.print(root.data + " ");
+            printInRange(root.right, k1, k2);
+        }else if(root.data < k1){
+            printInRange(root.right, k1, k2);
+        }else{
+            printInRange(root.left, k1, k2);
+        }
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for(int i=0; i<path.size(); i++){
+            System.out.print(path.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void root2Leaf(Node root, ArrayList<Integer> path){
+        if(root==null){
+            return;
+        }
+        path.add(root.data);
+        if(root.left == null && root.right==null){
+            printPath(path);
+        }
+
+        root2Leaf(root.left, path);
+        root2Leaf(root.right, path);
+        path.remove(path.size()-1);
+    }
+
+    public static boolean isValidBST(Node root, Node min, Node max){
+        if(root == null){
+            return true;
+        }
+
+        if(min != null && root.data <= min.data){
+            return false;
+        }
+        else if(max != null && root.data >= max.data){
+            return false;
+        }
+
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+
+    public static Node mirrorBST(Node root){
+        if(root ==null){
+            return null;
+        }
+
+        Node leftS = mirrorBST(root.left);
+        Node rightS = mirrorBST(root.right);
+        
+        root.left = rightS;
+        root.right = leftS;
+        return root;
+    }
+
+    public static void preOrder(Node root){
+        if(root == null){
+            return;
+        }
+
+        System.out.print(root.data+" ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
     
     public static void main(String args[]){
-        int values[] = {5,1,3,4,2,7};
+        int values[] = {8,5,3,6,10,11};
         Node root = null;
 
         for(int i=0; i<values.length; i++){
             root = insert(root, values[i]);
-        }
+        }   
+        
+        Node root1 = mirrorBST(root);
+        preOrder(root1);
 
-        int key = 1;
-        if(search(root, key)){
-            System.out.println("Found");
-        }else{
-            System.out.println("Not Found");
-        }
     }
 }
