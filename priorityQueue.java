1634782c9cef733936ae06514f39e6cc3bff33b2
Sumit Bhagat <sumitbhagat9266@gmail.com>
2025-09-25 11:10:15 +0530

    Heaps Initiated

---
+++ priorityQueue.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class priorityQueue {
+
+    static class Student implements Comparable<Student> {
+        String name;
+        int rank;
+
+        public Student(String name, int rank){
+            this.name = name;
+            this.rank = rank;
+        }
+
+        @Override 
+        public int compareTo(Student s2){
+            return this.rank - s2.rank;
+        }
+    }
+    public static void main(String args[]){
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student("A", 15000));
+        pq.add(new Student("B", 32453));
+        pq.add(new Student("C", 31));
+        pq.add(new Student("D", 1086367));
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek().name + "->" + pq.peek().rank);
+            pq.remove();
+        }
+    }
+}
